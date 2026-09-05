1a8c8aeb7997ba1107a6bd26c65a2804f651cd5f
nywm8rmpf4-sketch <nywm8rmpf4@privaterelay.appleid.com>
2026-09-05 08:42:59 +0200

    test(v3.1.9-A1.3R4): fix exported QA canonical path

--- qa-public/tests/v319-a13r3-tango-relation-path-cost.test.js
+++ qa-public/tests/v319-a13r3-tango-relation-path-cost.test.js
@@ -3,7 +3,7 @@
 const assert=require('assert');
 const fs=require('fs');
 const path=require('path');
-const ROOT=path.resolve(__dirname,'../..');
+const ROOT=path.resolve(__dirname,'..');
 const Planner=require(path.join(ROOT,'tango-played-move-planner.js'));
 const PlayedMoveRuntime=require(path.join(ROOT,'tango-played-move-runtime.js'));
 
